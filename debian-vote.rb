c08a5de8136c16da7c1d0906b5f808387b9ce8f3
Martin Hradil <mhradil@redhat.com>
2020-05-18 00:05:55 +0000

    debian-vote: add json feed

--- debian-vote.rb
+++ debian-vote.rb
@@ -1,6 +1,10 @@
 #!/usr/bin/env ruby
 # -*- encoding: utf-8 -*-
 
+FEED_TITLE = "Debian Voting Information"
+FEED_SOURCE = "https://www.debian.org/vote/"
+
+
 # Hpricot::Text to readable string without magic nbsp
 def str(s)
   raise "Parse error" unless Hpricot::Text === s
@@ -11,7 +15,7 @@ def items
   require 'hpricot'
   require 'open-uri'
 
-  menu = Hpricot(open("https://www.debian.org/vote/")) / "#second-nav > .votemenu"
+  menu = Hpricot(open(FEED_SOURCE)) / "#second-nav > .votemenu"
 
   sections = (menu / "> li").map do |li|
     {
@@ -31,29 +35,60 @@ def items
     end
   end
 
-  flat.reject { |item| ['Home', 'How To'].include?(item[:section]) }
+  filtered = flat.reject { |item| ['Home', 'How To'].include?(item[:section]) }
+
+  filtered.map do |item|
+    item.merge(:title => "#{item[:section]}: #{item[:title]}")
+  end
 end
 
-def feed(items)
+def atom_feed(items)
   require 'rss'
 
   RSS::Maker.make("atom") do |maker|
-    maker.channel.author = "Debian"
+    maker.channel.about = FEED_SOURCE
+    maker.channel.author = FEED_TITLE
+    maker.channel.title = FEED_TITLE
     maker.channel.updated = Time.now.to_s
-    maker.channel.about = "https://www.debian.org/vote/"
-    maker.channel.title = "Debian Voting Information"
 
     items.each do |item|
       maker.items.new_item do |new_item|
         new_item.link = item[:href]
-        new_item.title = "#{item[:section]}: #{item[:title]}"
+        new_item.title = item[:title]
         new_item.updated = Time.now.to_s
       end
     end
   end
 end
 
+def json_feed(items)
+  {
+    :version => "https://jsonfeed.org/version/1",
+    :home_page_url => FEED_SOURCE,
+    :title => FEED_TITLE,
+    :items => items.map do |item|
+      {
+        :content_text => item[:title],
+        :id => item[:href],
+        :url => item[:href],
+      }
+    end,
+  }
+end
 
-puts 'Content-Type: application/atom+xml'
-puts
-puts feed(items).to_s
+def atom
+  puts 'Content-Type: application/atom+xml'
+  puts
+  puts atom_feed(items).to_s
+end
+
+def json
+  require 'json'
+
+  puts 'Content-Type: application/json'
+  puts
+  puts JSON.pretty_generate(json_feed(items))
+end
+
+#atom
+json
